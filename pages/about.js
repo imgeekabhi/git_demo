516dcd5a3743e64387f48a07e5c4dd6296ad66cd
Abhishek <imgeekabhi@gmail.com>
2023-02-12 11:23:42 +0530

    added navbar

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import Navbar from '../Components/Navbar';
 const about = () => {
-  return <div>about page</div>;
+  return (
+    <div>
+      about page
+      <Navbar />
+    </div>
+  );
 };
 
 export default about;
